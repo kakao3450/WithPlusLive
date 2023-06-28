48de7923b71ea76d5a912f9df463e715e01cfd70
gkcksrbs <70510732+gkcksrbs@users.noreply.github.com>
2023-06-28 15:26:26 +0900

    add kakao config

---
+++ src/config/kakao.js
@@ -0,0 +1,7 @@
+const kakao = {
+  JAVASCRIPT_KEY: "effabfca853c30c852053e4b44efd5c2",
+  REST_API_KEY: "e79fc5994ea6cf220c38cfb994da9b9c",
+  REDIRECT_PAGE: "http://localhost:3000/oauth/kakao",
+};
+
+export default kakao;
